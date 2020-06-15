4c3f27f7e3371d6e84734ce31da13db67821f073
Branden Keck <bmkeck62@gmail.com>
2020-06-14 22:28:05 -0400

    Added "forest1" build for Drone Sim / Init "forest 2"

--- RL-Bot/Assets/Drone-Sim/Scripts/DroneMove.cs
+++ RL-Bot/Assets/Drone-Sim/Scripts/DroneMove.cs
@@ -9,11 +9,15 @@ public class DroneMove : MonoBehaviour
     public float liftVelocityCap = 10;
     public float moveVelocityCap = 10;
     public float rotateVelocityCap = 10;
+    public float pitchVelocityCap = 10;
+    public float rollVelocityCap = 10;
 
     private Rigidbody rb;
     private float liftVelocity;
     private float moveVelocity;
     private float rotateVelocity;
+    private float pitchVelocity;
+    private float rollVelocity;
 
     private void Start()
     {
@@ -21,24 +25,39 @@ public class DroneMove : MonoBehaviour
         liftVelocity = 0;
         moveVelocity = 0;
         rotateVelocity = 0;
+        pitchVelocity = 0;
+        rollVelocity = 0;
     }
 
     void FixedUpdate()
     {
-        bool lift = Input.GetKey(KeyCode.Space);
-        bool forward = Input.GetKey(KeyCode.UpArrow);
-        bool backward = Input.GetKey(KeyCode.DownArrow);
-        bool rotateLeft = Input.GetKey(KeyCode.LeftArrow);
-        bool rotateRight = Input.GetKey(KeyCode.RightArrow);
+        bool up = Input.GetKey(KeyCode.Q);
+        bool down = Input.GetKey(KeyCode.E);
+        bool forward = Input.GetKey(KeyCode.W);
+        bool backward = Input.GetKey(KeyCode.S);
+        bool rotateLeft = Input.GetKey(KeyCode.A);
+        bool rotateRight = Input.GetKey(KeyCode.D);
+        bool pitchForward = Input.GetKey(KeyCode.UpArrow);
+        bool pitchBackward = Input.GetKey(KeyCode.DownArrow);
+        bool rollLeft = Input.GetKey(KeyCode.LeftArrow);
+        bool rollRight = Input.GetKey(KeyCode.RightArrow);
 
-        if (lift)
+        if (up)
         {
             liftVelocity = liftVelocity + speedFactor;
             if (liftVelocity > liftVelocityCap)
             {
                 liftVelocity = liftVelocityCap;
             }
-        }else
+        }else if (down)
+        {
+            liftVelocity = liftVelocity - speedFactor;
+            if (liftVelocity < -1*liftVelocityCap)
+            {
+                liftVelocity = -1*liftVelocityCap;
+            }
+        }
+        else
         {
             liftVelocity = 0;
         }
@@ -81,8 +100,59 @@ public class DroneMove : MonoBehaviour
             rotateVelocity = 0;
         }
 
+        if (pitchBackward)
+        {
+            pitchVelocity = pitchVelocity - 100 * speedFactor;
+            if (pitchVelocity < -1 * pitchVelocityCap)
+            {
+                pitchVelocity = -1 * pitchVelocityCap;
+            }
+        }
+        else if (pitchForward)
+        {
+            pitchVelocity = pitchVelocity + 100 * speedFactor;
+            if (pitchVelocity > pitchVelocityCap)
+            {
+                pitchVelocity = pitchVelocityCap;
+            }
+        }
+        else
+        {
+            pitchVelocity = 0;
+        }
+
+        if (rollRight)
+        {
+            rollVelocity = rollVelocity - 100 * speedFactor;
+            if (rollVelocity < -1 * rollVelocityCap)
+            {
+                rollVelocity = -1 * rollVelocityCap;
+            }
+        }
+        else if (rollLeft)
+        {
+            rollVelocity = rollVelocity + 100 * speedFactor;
+            if (rollVelocity > rollVelocityCap)
+            {
+                rollVelocity = rollVelocityCap;
+            }
+        }
+        else
+        {
+            rollVelocity = 0;
+        }
+
         transform.Translate(new Vector3(moveVelocity, liftVelocity, 0) * Time.deltaTime);
-        transform.Rotate(new Vector3(0, rotateVelocity, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(rollVelocity, rotateVelocity, pitchVelocity) * Time.deltaTime);
+
+        if(!up && !down && !forward && !backward)
+        {
+            rb.velocity = Vector3.zero;
+        }
+        if (!rotateLeft && !rotateRight && !pitchForward && !pitchBackward && !rollLeft && !rollRight)
+        {
+            rb.angularVelocity = Vector3.zero;
+        }
 
     }
 }
